999afc86b5bd0f2ff2c146a30cce450cb45f7f74
lauttigl <ljiglesias@outlook.com>
2023-01-13 22:46:53 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import {ItemDetail} from './Components/Item/ItemDetail'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5i9oyG1D24rJQ6r4SnpDqFCrtrZeYCdc",
-  authDomain: "techcommerce-3e5c9.firebaseapp.com",
-  projectId: "techcommerce-3e5c9",
-  storageBucket: "techcommerce-3e5c9.appspot.com",
-  messagingSenderId: "602920689238",
-  appId: "1:602920689238:web:1d6f1f238dc61052eb25f7"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 // Initialize Firebase
